7ae4555fedf62e0045baeb53bd62d113f6b58634
dimmoborgir <dimmoborgir@gmail.com>
2014-03-08 11:03:21 +0600

    1. scores swap: adc becomes hardware:300, hw_dump becomes hardware:200 2. adc: valid keys, temporarily internal ip in checker 3. hw_reverse: xml-checker stub (will be replaced later)

--- tasks/adc/source/server/gen-keys.pl
+++ tasks/adc/source/server/gen-keys.pl
@@ -1,10 +1,10 @@
 use strict;
 use Digest::MD5 qw(md5_hex);
 
-my $salt = 'oiuHYOIUgshadkwqfdshgfdsafdsaf';
-my $rnd = int rand 65536;
+my $salt = '+VseChtoUgodno+';
+my $taskId = 'hardware:300';
 
 $\=$/;
 
 my $count = shift || 300;
-print md5_hex($salt.$rnd.$_) for 1..$count;
+print md5_hex($taskId.$_.$salt) for 1..$count;
